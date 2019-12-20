179889058d704867175c1994ff32dae0da74cf64
baoweike666 <36069600+baoweike666@users.noreply.github.com>
2019-12-20 21:01:06 +0800

    Update ppm.v

--- ppm.v
+++ ppm.v
@@ -12,7 +12,7 @@ wire d_en, f_en;
 reg [2:0] count;
 reg [2:0] state;
 
-//shift register
+// shift register
 always @(posedge clk or negedge rst) begin
 	if(!rst)	shiftreg <= 8'h00;
 	else	begin
@@ -27,51 +27,52 @@ always @(posedge clk or negedge rst) begin
 	end
 end
 
-//controller
+// state machine
 always @(posedge clk or negedge rst) begin
 	if(!rst) begin
 		state <= 3'd0;
 	end
 	else begin
+		// state 0,1,2 shift
 		case(state)
-		3'd0: // idle
-			if(shiftreg==8'b0111_1011) state <= 3'd1;
-			else state <= 3'd0;
-		3'd1: // SOF
-			state <= 3'd2;
-		3'd2: // DATA
-			if(shiftreg == 8'b1101_0001) state <= 3'd0;
-			else state <= 3'd2;
-		default: state <= 3'd0;
+		3'd0:// state 0
+			if(shiftreg==8'b0111_1011) state <= 3'd1;// SOF, 0 to 1
+			else state <= 3'd0;// keep state 0
+		3'd1:// state 1
+			state <= 3'd2;// 1 to 2
+		3'd2:// state 2
+			if(shiftreg == 8'b1101_0001) state <= 3'd0;// EOF, 2 to 0
+			else state <= 3'd2;// keep state 2
+		default: state <= 3'd0;// default, state 0
 		endcase
 	end
 end
 
-//counter
+// counter
 always @(posedge clk or negedge rst) begin
 	if(!rst) begin
 		count <= 3'd0;
 	end
 	else begin
-		if (state==3'd0 && shiftreg==8'b0111_1011) count <= 3'd0;
-		else count <= count + 3'd1;
+		if (state==3'd0 && shiftreg==8'b0111_1011) count <= 3'd0;// state 0 & SOF, count 0
+		else count <= count + 3'd1;// counting
 	end
 end
 
 assign f_en = (state==3'd1) ? 1'b1 : 1'b0;
 assign d_en = (state==3'd2 && count==3'd0) ? 1'b1 : 1'b0;
 
-//decoder
+// decoder
 always @(posedge clk or negedge rst) begin
 	if(!rst) begin
 		dout <= 2'b00;
 	end
 	else if(state==3'd2 && count==3'd7) begin
 		case(shiftreg)
-		8'b1011_1111: dout <= 2'b00;
-		8'b1110_1111: dout <= 2'b01;
-		8'b1111_1011: dout <= 2'b10;
-		8'b1111_1110: dout <= 2'b11;
+		8'b1011_1111: dout <= 2'b00;// decode as 00
+		8'b1110_1111: dout <= 2'b01;// decode as 01
+		8'b1111_1011: dout <= 2'b10;// decode as 10
+		8'b1111_1110: dout <= 2'b11;// decode as 11
 		default: dout <= 2'b00;
 		endcase
 	end
